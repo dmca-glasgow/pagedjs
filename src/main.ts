01c1d9339cd4e9b905fdbc249be7fd8917dcec9a
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2024-10-26 10:02:52 +0100

    pagedjs try 1: using the recommended initialisation code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,42 @@
+// @ts-expect-error
+import { Previewer } from 'pagedjs';
+
 const app = document.querySelector<HTMLDivElement>('.app');
 const print = document.querySelector<HTMLDivElement>('.print');
 const webBtn = document.querySelector<HTMLButtonElement>('.web-btn')
 const printBtn = document.querySelector<HTMLButtonElement>('.print-btn')
+
+const config = {
+	auto: true,
+	before: undefined,
+	after: undefined,
+	content: app,
+	stylesheets: undefined,
+	renderTo: print,
+	settings: undefined
+};
+
+const previewer = new Previewer(config.settings);
+
+new Promise((resolve) => {
+	if (document.readyState === "interactive" || document.readyState === "complete") {
+		resolve(document.readyState);
+		return;
+	}
+	document.onreadystatechange = () => {
+		if (document.readyState === "interactive") {
+			resolve(document.readyState);
+		}
+	};
+}).then(async function () {
+  const chunker = await previewer.preview(
+    config.content,
+    undefined,
+    config.renderTo
+  );
+  // console.log(chunker.pages)
+  console.log(`took: ${(chunker.performance / 1000).toFixed(2)}s`)
+});
 
 webBtn?.addEventListener('click', () => {
   webBtn.classList.add('active')
